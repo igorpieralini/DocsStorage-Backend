d4caeae5bec8880d612d8f0b42a0ef90150a9398
Igor Pieralini <igorpieralini@gmail.com>
2025-11-29 00:02:21 -0300

    small ajust service google drive

--- app/services/gdrive_service.py
+++ app/services/gdrive_service.py
@@ -1,47 +1,43 @@
 import os
 import io
-from google.oauth2 import service_account
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload, MediaIoBaseDownload
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+CREDENTIALS_DIR = "credentials"
+CLIENT_SECRET_FILE = os.path.join(CREDENTIALS_DIR, "client_secret.json")
+TOKEN_FILE = os.path.join(CREDENTIALS_DIR, "token.json")
 
-class GDriveService:
-    def __init__(self, credentials_dir):
-        self.credentials_dir = credentials_dir
-        self.service = self._init_service()
+class GDrive:
+    def __init__(self):
+        self.service = self._authenticate()
 
-    def _init_service(self):
-        sa_path = os.path.join(self.credentials_dir, "service_account.json")
-        client_secret_path = os.path.join(self.credentials_dir, "client_secret.json")
-        token_path = os.path.join(self.credentials_dir, "token.json")
+    def _authenticate(self):
+        creds = None
 
-        if os.path.exists(sa_path):
-            creds = service_account.Credentials.from_service_account_file(sa_path, scopes=SCOPES)
-            return build("drive", "v3", credentials=creds)
-        elif os.path.exists(token_path):
-            creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+        if os.path.exists(TOKEN_FILE):
+            creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+
+        if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
-            return build("drive", "v3", credentials=creds)
-        elif os.path.exists(client_secret_path):
-            flow = InstalledAppFlow.from_client_secrets_file(client_secret_path, SCOPES)
-            creds = flow.run_local_server(port=0)
-            with open(token_path, "w") as f:
-                f.write(creds.to_json())
-            return build("drive", "v3", credentials=creds)
-        else:
-            raise FileNotFoundError("Nenhuma credencial do Google Drive encontrada em credentials/")
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+                creds = flow.run_local_server(port=0)
 
-    def upload_file(self, file_stream, filename, mimetype=None, parent_folder_id=None):
-        file_metadata = {"name": filename}
-        if parent_folder_id:
-            file_metadata["parents"] = [parent_folder_id]
+            with open(TOKEN_FILE, "w") as token:
+                token.write(creds.to_json())
+        return build("drive", "v3", credentials=creds)
+
+    def upload_file(self, file_stream, filename, mimetype="application/octet-stream", folder_id=None):
+        metadata = {"name": filename}
+        if folder_id:
+            metadata["parents"] = [folder_id]
         media = MediaIoBaseUpload(file_stream, mimetype=mimetype, resumable=True)
-        file = self.service.files().create(body=file_metadata, media_body=media, fields="id, name").execute()
+        file = self.service.files().create(body=metadata, media_body=media, fields="id, name").execute()
         return file.get("id")
 
     def download_file(self, file_id):
@@ -50,6 +46,6 @@ class GDriveService:
         downloader = MediaIoBaseDownload(fh, request)
         done = False
         while not done:
-            status, done = downloader.next_chunk()
+            _, done = downloader.next_chunk()
         fh.seek(0)
         return fh
